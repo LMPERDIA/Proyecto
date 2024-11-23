f3c209b956615f08113a284a8cbd1766f0d419d7
JoseSuarezG <josed_5@hotmail.com>
2024-11-23 18:28:18 -0500

    Actualizacion de la visualizacion para consumir la API

--- visualizacion/visualización_v2_1011.py
+++ visualizacion/visualización_v2_1011.py
@@ -18,6 +18,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import plotly.express as px
+import requests
 
 # Cargar el dataset y entrenar el modelo
 data = pd.read_csv('diamonds.csv')
@@ -42,39 +43,84 @@ app = dash.Dash(__name__)
 app.layout = html.Div([
     # Portada
     html.Div([
-        html.H1("VALDI", style={'text-align': 'center', 'font-size': '48px'}),
-        html.H2("Valuador de Diamantes Inteligente", style={'text-align': 'center', 'font-size': '24px'})
+        html.H1("VALDI", style={'text-align': 'center', 'font-size': '48px', 'fontfamily':'Impact'}),
+        html.H2("VALuador de Diamantes Inteligente", style={'text-align': 'center', 'font-size': '24px', 'fontfamily':'Impact'})
     ], style={'padding': '40px', 'background-color': '#f0f0f0'}),
 
     # Formulario de entrada de características (Sección 1)
     html.Div([
         html.H3("Ingrese las características del diamante", style={'text-align': 'center'}),
-        html.Label("Peso en quilates (Carat)"),
-        dcc.Input(id="input-carat", type="number", placeholder="Peso en quilates", step=0.01),
 
+        html.Div([
+        html.Label("Peso en quilates (Carat)"),
+        dcc.Input(id="input-carat", type="number", placeholder="Peso en quilates", step=0.01)]
+        ,style={'marginBottom': '20px'}),
+
+        html.Div([
+        html.Label("Total porcentaje de profundidad (depth)"),
+        dcc.Input(id="input-depth", type="number", placeholder="43--79 %", step=0.01)]
+        ,style={'marginBottom': '20px'}),
+
+        html.Div([
+        html.Label("Ancho del tope del diamante (table)"),
+        dcc.Input(id="input-table", type="number", placeholder="43--79", step=0.01)]
+        ,style={'marginBottom': '20px'}),
+
+        html.Div([
+        html.Label("Largo del diamante (X)"),
+        dcc.Input(id="input-X", type="number", placeholder="milimetros", step=0.01)]
+        ,style={'marginBottom': '20px'}),
+
+        html.Div([
+        html.Label("Ancho del diamante (Y)"),
+        dcc.Input(id="input-Y", type="number", placeholder="milimetros", step=0.01)]
+        ,style={'marginBottom': '20px'}),
+
+        html.Div([
+        html.Label("Profundidad del diamante (Z)"),
+        dcc.Input(id="input-Z", type="number", placeholder="milimetros", step=0.01)]
+        ,style={'marginBottom': '20px'}),
+
+        html.Div([
         html.Label("Corte (Cut)"),
         dcc.Dropdown(
             id="input-cut",
-            options=[{'label': i, 'value': label_encoders['cut'].transform([i])[0]} for i in label_encoders['cut'].classes_],
-            placeholder="Seleccione el corte"
-        ),
+            options=[{'label': 'Good', 'value': 'Good'},
+                    {'label': 'Ideal', 'value': 'Ideal'},
+                    {'label': 'Premium', 'value': 'Premium'},
+                    {'label': 'Very Good', 'value': 'VeryGood'}],
+            placeholder="Seleccione el corte")]
+        ,style={'marginBottom': '20px'}),
 
+        html.Div([
         html.Label("Color"),
         dcc.Dropdown(
             id="input-color",
-            options=[{'label': i, 'value': label_encoders['color'].transform([i])[0]} for i in label_encoders['color'].classes_],
-            placeholder="Seleccione el color"
-        ),
+            options=[{'label': 'Color E', 'value': 'E'},
+                    {'label': 'Color F', 'value': 'F'},
+                    {'label': 'Color G', 'value': 'G'},
+                    {'label': 'Color H', 'value': 'H'},
+                    {'label': 'Color I', 'value': 'I'},
+                    {'label': 'Color J', 'value': 'J'}],
+            placeholder="Seleccione el color")]
+        ,style={'marginBottom': '20px'}),
 
+        html.Div([
         html.Label("Claridad (Clarity)"),
         dcc.Dropdown(
             id="input-clarity",
-            options=[{'label': i, 'value': label_encoders['clarity'].transform([i])[0]} for i in label_encoders['clarity'].classes_],
-            placeholder="Seleccione la claridad"
-        ),
+            options=[{'label': 'Claridad IF', 'value': 'IF'},
+                    {'label': 'Claridad SI1', 'value': 'SI1'},
+                    {'label': 'Claridad SI2', 'value': 'SI2'},
+                    {'label': 'Claridad VS1', 'value': 'VS1'},
+                    {'label': 'Claridad VS2', 'value': 'VS2'},
+                    {'label': 'Claridad VVS1', 'value': 'VVS1'},
+                    {'label': 'Claridad VVS2', 'value': 'VVS2'}],
+            placeholder="Seleccione la claridad")]
+        ,style={'marginBottom': '20px'}),
 
         html.Button("Predecir Precio", id="predict-button", n_clicks=0, style={'margin-top': '20px'}),
-    ], style={'padding': '20px', 'text-align': 'center'}),
+    ], style={'width': '50%', 'margin': '0 auto', 'padding': '20px'}), #style={'padding': '20px', 'text-align': 'center'}),
 
     # Resultado de la predicción y características del diamante (Sección 2)
     html.Div(id="output-section", style={'padding': '20px', 'text-align': 'center', 'font-size': '24px'}),
@@ -93,22 +139,73 @@ app.layout = html.Div([
     Input("predict-button", "n_clicks"),
     [
         Input("input-carat", "value"),
+        Input("input-depth", "value"),
+        Input("input-table", "value"),
+        Input("input-X", "value"),
+        Input("input-Y", "value"),
+        Input("input-Z", "value"),
         Input("input-cut", "value"),
         Input("input-color", "value"),
         Input("input-clarity", "value")
     ]
 )
-def predict_price(n_clicks, carat, cut, color, clarity):
+def predict_price(n_clicks, carat, depth, table, X, Y, Z, cut, color, clarity):
     # Verifica que haya un clic y que todas las entradas no sean None
-    if n_clicks > 0 and carat is not None and cut is not None and color is not None and clarity is not None:
+    if n_clicks > 0 and carat is not None and depth is not None and table is not None and X is not None and Y is not None and Z is not None and cut is not None and color is not None and clarity is not None:
+        # Se transforman los datos para el modelo
+        match cut:
+            case 'Good':
+                cutArray = [1,0,0,0]
+            case 'Ideal':
+                cutArray = [0,1,0,0]
+            case 'Premium':
+                cutArray = [0,0,1,0]
+            case 'VeryGood':
+                cutArray = [0,0,0,1]
+        match color:
+            case 'E':
+                colorArray = [1,0,0,0,0,0]
+            case 'F':
+                colorArray = [0,1,0,0,0,0]
+            case 'G':
+                colorArray = [0,0,1,0,0,0]
+            case 'H':
+                colorArray = [0,0,0,1,0,0]
+            case 'I':
+                colorArray = [0,0,0,0,1,0]
+            case 'J':
+                colorArray = [0,0,0,0,0,1]
+        match clarity:
+            case 'IF':
+                clarityArray = [1,0,0,0,0,0,0]
+            case 'SI1':
+                clarityArray = [0,1,0,0,0,0,0]
+            case 'SI2':
+                clarityArray = [0,0,1,0,0,0,0]
+            case 'VS1':
+                clarityArray = [0,0,0,1,0,0,0]
+            case 'VS2':
+                clarityArray = [0,0,0,0,1,0,0]
+            case 'VVS1':
+                clarityArray = [0,0,0,0,0,1,0]
+            case 'VVS2':
+                clarityArray = [0,0,0,0,0,0,1]
         try:
             # Intenta hacer la predicción
-            features = [[carat, cut, color, clarity, 61, 55, 4, 4, 2]]
-            predicted_price = model.predict(features)[0]
+            features = list([carat, depth, table, X, Y, Z])
+            features.extend(cutArray)
+            features.extend(colorArray)
+            features.extend(clarityArray)
+            # Se hace la solicitud a la API
+            headers_API = {'Content-Type': 'application/json'}
+            data_API = {'features':[0.23, 61.5, 55.0, 326, 3.95, 3.98, 2.43, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1 ,1]}
+            #predicted_price = model.predict(features)[0]
+            predictedPrice = requests.post('http://54.86.72.39:8000/predict',headers=headers_API,json=data_API)
+            predict = predictedPrice.json()['prediction'][0]
             # Devuelve la estructura HTML con el precio estimado y características
             return [
                 html.H3("Predicción de Precio", style={'text-align': 'center'}),
-                html.P(f"Precio estimado: ${predicted_price:.2f} USD", style={'font-size': '32px', 'font-weight': 'bold'}),
+                html.P(f"Precio estimado: ${predict:.2f} USD", style={'font-size': '32px', 'font-weight': 'bold'}),
                 html.Hr(),
                 html.H4("Características ingresadas"),
                 html.P(f"Carat: {carat}, Cut: {cut}, Color: {color}, Clarity: {clarity}")
